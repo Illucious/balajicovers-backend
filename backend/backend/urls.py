7d2c28b096fd0e958ebc7415d3883e417115280e
Illucious <58263593+Illucious@users.noreply.github.com>
2023-10-04 16:35:06 +0530

    added .env

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,11 +18,11 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/auth/', include('authentication.urls'), name='authentication'),
-    path('api/store/', include('store.urls'), name='store'),
-    path('api/checkout/', include('checkout.urls'), name='checkout'),
-    path('api/customize/', include('customize.urls'), name='customize'),
-    path('api/feedback/', include('feedback.urls'), name='feedback'),
-    path('api/reviews/', include('reviews.urls'), name='reviews'),
+    path("admin/", admin.site.urls),
+    # path("api/auth/", include("authentication.urls"), name="authentication"),
+    # path("api/store/", include("store.urls"), name="store"),
+    # path("api/checkout/", include("checkout.urls"), name="checkout"),
+    # path("api/customize/", include("customize.urls"), name="customize"),
+    # path("api/feedback/", include("feedback.urls"), name="feedback"),
+    # path("api/reviews/", include("reviews.urls"), name="reviews"),
 ]
