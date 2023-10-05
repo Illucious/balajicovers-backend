4cb96c8516259ee16eb163674979afc5d0887e95
Illucious <58263593+Illucious@users.noreply.github.com>
2023-10-05 20:03:08 +0530

    black formatting

--- backend/store/serializers.py
+++ backend/store/serializers.py
@@ -1,19 +1,20 @@
 from rest_framework import serializers
 from .models import Categories, SubCategories, Products
 
+
 class CategoriesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categories
-        fields = '__all__'
+        fields = "__all__"
 
 
 class SubCategoriesSerializer(serializers.ModelSerializer):
     class Meta:
         model = SubCategories
-        fields = '__all__'
+        fields = "__all__"
 
 
 class ProductsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Products
-        fields = '__all__'
+        fields = "__all__"
