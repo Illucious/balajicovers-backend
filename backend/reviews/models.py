28fd64df0ecff244c9befaa47a23413334b94999
Illucious <58263593+Illucious@users.noreply.github.com>
2023-10-06 10:57:36 +0530

    implimented JWT Auth

--- backend/reviews/models.py
+++ backend/reviews/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-from store.models import Product
+from store.models import Products
 
 # Create your models here.
 
 
 class Review(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
     user_name = models.CharField(max_length=50)
     review_title = models.CharField(max_length=100)
     review_text = models.CharField(max_length=500)
