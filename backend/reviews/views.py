5d33a0c583423616f7778b20185e23c581b28785
Illucious <58263593+Illucious@users.noreply.github.com>
2023-10-08 15:10:29 +0530

    made review end points(need update)

--- backend/reviews/views.py
+++ backend/reviews/views.py
@@ -3,24 +3,24 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.decorators import api_view, permission_classes
 
-from .models import ProductReview
-from .serializers import ProductReviewSerializer
+from .models import Review
+from .serializers import ReviewSerializer
 
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticatedOrReadOnly])
 def product_review_list_create(request, product_id=None):
     if request.method == 'GET':
         if product_id is not None:
-            reviews = ProductReview.objects.filter(product_id=product_id)
+            reviews = Review.objects.filter(product_id=product_id)
             if not reviews.exists():
                 return Response({"error": "No reviews found for the specified product."}, status=status.HTTP_404_NOT_FOUND)
-            serializer = ProductReviewSerializer(reviews, many=True)
+            serializer = ReviewSerializer(reviews, many=True)
             return Response(serializer.data)
         else:
             return Response({"error": "Product ID must be provided."}, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'POST':
-        serializer = ProductReviewSerializer(data=request.data)
+        serializer = ReviewSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
