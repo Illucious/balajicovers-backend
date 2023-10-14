f5475b8bb5f62929a16820489dd6df2916f90b25
Illucious <58263593+Illucious@users.noreply.github.com>
2023-10-14 11:59:53 +0530

    made checkout end points

--- backend/checkout/urls.py
+++ backend/checkout/urls.py
@@ -3,4 +3,9 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = []
+urlpatterns = [
+    path("cart/", views.get_cart, name="get_cart"),
+    path("cart/delete/", views.delete_cart, name="delete_cart"),
+    path("cart/add/", views.add_to_cart, name="add_to_cart"),
+    path("order/", views.place_order, name="place_order"),
+]
--- backend/checkout/views.py
+++ backend/checkout/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .models import Cart, Order
+from store.models import Products
 from .serializers import CartSerializer, OrderSerializer
 
 # Create your views here.
@@ -37,3 +39,32 @@ def add_to_cart(request):
 
 
 # order views here
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def place_order(request):
+
+    cart = Cart.objects.get(user=request.user)
+    if cart is not None:
+        cart.checkout = True
+        cart.save()
+        items = cart.items
+
+        total = 0
+        for item in items:
+            product = Products.objects.get(id=item)
+            total += product.price
+
+        serializer = OrderSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user, items=items, total=total)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        item = Products.objects.get(id=request.data["items"])
+        total = item.price
+        serializer = OrderSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user, items=item, total=total)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
