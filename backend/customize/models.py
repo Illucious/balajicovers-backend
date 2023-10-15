eeb5d75bd5b1d0a79c3a41eff48e74bb10b94e63
Illucious <58263593+Illucious@users.noreply.github.com>
2023-10-15 17:27:58 +0530

    connected models to admin

--- backend/customize/models.py
+++ backend/customize/models.py
@@ -8,7 +8,7 @@ from django.contrib.postgres.fields import ArrayField
 class Customize(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="images/customize")
-    name_on_cover = models.CharField(max_length=100)
-    quantity = models.IntegerField()
+    name_on_cover = models.CharField(max_length=100, default=None)
+    quantity = models.IntegerField(default=1)
     ammount = models.DecimalField(max_digits=10, decimal_places=2)
-    phone = models.CharField(max_length=10)
+    phone = models.CharField(max_length=10, default=None)
