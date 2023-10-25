130572b1330b7bbb9c2e18e4475ede434bd53766
Illucious <58263593+Illucious@users.noreply.github.com>
2023-10-25 23:33:58 +0530

    added jet admin pannel for ease of client

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("jet/", include("jet.urls")),
+    path("jet/dashboard/", include("jet.dashboard.urls", "jet-dashboard")),
     path("admin/", admin.site.urls),
     path("api/auth/", include("authentication.urls"), name="authentication"),
     path("api/store/", include("store.urls"), name="store"),
